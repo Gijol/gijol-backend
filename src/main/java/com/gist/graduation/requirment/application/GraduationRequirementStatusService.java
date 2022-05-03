44dc2ce4bb236e08239cacbc43908d08155fde15
grace_goose <choieungi@gm.gist.ac.kr>
2022-05-03 12:07:55 +0900

    fix: change classresource to inputstream

--- src/main/java/com/gist/graduation/requirment/application/GraduationRequirementStatusService.java
+++ src/main/java/com/gist/graduation/requirment/application/GraduationRequirementStatusService.java
@@ -5,24 +5,34 @@ import com.gist.graduation.requirment.domain.MajorType;
 import com.gist.graduation.requirment.dto.GradeToCheckRequest;
 import com.gist.graduation.user.taken_course.UserTakenCoursesList;
 import com.gist.graduation.utils.UserTakenCousrseParser;
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 public class GraduationRequirementStatusService {
 
     public GraduationRequirementStatus checkGraduationCondition(GradeToCheckRequest request) throws IOException {
-        File file = request.getMultipartFile().getResource().getFile();
+        File file = multiPartToFile(request.getMultipartFile());
         UserTakenCoursesList userTakenCoursesList = UserTakenCousrseParser.parseUserTakenCousrse(file);
         Integer studentId = UserTakenCousrseParser.getStudentId(file);
         GraduationRequirementStatus graduationRequirementStatus = new GraduationRequirementStatus();
         graduationRequirementStatus.checkGraduationRequirements(studentId, userTakenCoursesList, request.getMajorType());
         return graduationRequirementStatus;
 
+    }
+
+    private File multiPartToFile(MultipartFile multipartFile) throws IOException {
+        InputStream inputStream = multipartFile.getInputStream();
+        File file = File.createTempFile(UUID.randomUUID().toString(), ".xlsx");
+        FileUtils.copyToFile(inputStream, file);
+        return file;
     }
 
     public GraduationRequirementStatus checkGradeTest(File file, MajorType majorType) throws IOException {

694be4870eafe804de6e23d585cd60f39ef9af10
grace_goose <choieungi@gm.gist.ac.kr>
2022-05-05 14:05:07 +0900

    feature: for test

--- src/main/java/com/gist/graduation/requirment/presentation/TestController.java
+++ src/main/java/com/gist/graduation/requirment/presentation/TestController.java
@@ -2,7 +2,9 @@ package com.gist.graduation.requirment.presentation;
 
 import com.gist.graduation.requirment.application.GraduationRequirementStatusService;
 import com.gist.graduation.requirment.domain.GraduationRequirementStatus;
+import com.gist.graduation.requirment.domain.MajorType;
 import com.gist.graduation.requirment.dto.GradeToCheckRequest;
+import com.gist.graduation.user.taken_course.UserTakenCoursesList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +21,6 @@ public class TestController {
 
     private final GraduationRequirementStatusService graduationRequirementStatusService;
 
-
     @GetMapping("/upload")
     public String formTest(@ModelAttribute GradeToCheckRequest request){
         return "upload";
@@ -32,5 +33,11 @@ public class TestController {
         return "result";
     }
 
-
+    @GetMapping("/empty")
+    public String formEmptyTest(Model model) throws IOException {
+        GraduationRequirementStatus graduationRequirementStatus = new GraduationRequirementStatus();
+        graduationRequirementStatus.checkGraduationRequirements(20, new UserTakenCoursesList(), MajorType.EC);
+        model.addAttribute("result", graduationRequirementStatus);
+        return "result";
+    }
 }

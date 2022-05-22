7b9685f6268033a78874e3afea2e56f3d4eb1dc1
grace_goose <choieungi@gm.gist.ac.kr>
2022-05-22 17:41:00 +0900

    feat: feedback endpoint (#29)

--- src/main/java/com/gist/graduation/requirment/presentation/GraduationRequirementStatusController.java
+++ src/main/java/com/gist/graduation/requirment/presentation/GraduationRequirementStatusController.java
@@ -3,14 +3,18 @@ package com.gist.graduation.requirment.presentation;
 import com.gist.graduation.requirment.application.GraduationRequirementStatusService;
 import com.gist.graduation.requirment.domain.GraduationRequirementStatus;
 import com.gist.graduation.requirment.domain.major.MajorType;
+import com.gist.graduation.requirment.dto.FeedbackRequest;
 import com.gist.graduation.requirment.dto.GradeToCheckRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.URI;
 
 @RequiredArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping("/graduation")
 public class GraduationRequirementStatusController {
@@ -21,6 +25,12 @@ public class GraduationRequirementStatusController {
     public ResponseEntity<GraduationRequirementStatus> checkGraduation(GradeToCheckRequest request) throws IOException {
         GraduationRequirementStatus graduationRequirementStatus = graduationRequirementStatusService.checkGraduationCondition(request);
         return ResponseEntity.ok().body(graduationRequirementStatus);
+    }
+
+    @PostMapping("/feedback")
+    public ResponseEntity<Void> getFeedback(@RequestBody FeedbackRequest request){
+        log.info("[feedback] " +request.getMessage());
+        return ResponseEntity.created(URI.create("")).build();
     }
 
     @GetMapping("/test")

0b9ba6a52a79ccd41e740410c5b4b7ee8988138a
grace_goose <choieungi@gm.gist.ac.kr>
2023-05-17 22:28:36 +0900

    feat: calculator 구현

---
+++ src/main/java/com/gist/graduation/user/application/GraduationCalculator.java
@@ -0,0 +1,42 @@
+package com.gist.graduation.user.application;
+
+import com.gist.graduation.user.domain.UserTakenCourse;
+import com.gist.graduation.user.domain.vo.LetterGrade;
+import com.gist.graduation.user.dto.UserTakenCoursesAndGradeResponse.UserTakenCourseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class GraduationCalculator {
+
+    public BigDecimal calculateAverageGradeBySemester(List<UserTakenCourseResponse> userTakenCourses) {
+        BigDecimal totalCreditAndGrade = userTakenCourses.stream()
+                .map(UserTakenCourseResponse::multiplyCreditAndGrade)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal totalCredit = userTakenCourses.stream()
+                .map(UserTakenCourseResponse::getCredit)
+                .map(BigDecimal::new)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return totalCreditAndGrade.divide(totalCredit, 3, RoundingMode.DOWN);
+    }
+
+    public BigDecimal calculateTotalAverageGrade(List<UserTakenCourse> userTakenCourses) {
+        BigDecimal totalCreditAndGrade = userTakenCourses.stream()
+                .map(UserTakenCourse::multiplyCreditAndGrade)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal totalCredit = userTakenCourses.stream()
+                .map(UserTakenCourse::getCredit)
+                .map(BigDecimal::new)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return totalCreditAndGrade.divide(totalCredit, 3, RoundingMode.DOWN);
+    }
+}

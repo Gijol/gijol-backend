bd80f59a7df2bfcf9e82b4d223703a037086d23e
grace_goose <choieungi@gm.gist.ac.kr>
2023-06-03 19:47:00 +0900

    fix: aesConverter 코드 수정

--- src/main/java/com/gist/graduation/utils/converter/AesConverter.java
+++ src/main/java/com/gist/graduation/utils/converter/AesConverter.java
@@ -1,46 +1,38 @@
 package com.gist.graduation.utils.converter;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import java.security.Key;
-import java.util.Base64;
 
 @Converter
 @Component
+@RequiredArgsConstructor
 public class AesConverter implements AttributeConverter<String, String> {
 
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
-
-    @Value("{aes.key:key}")
-    private String key;
-
+    private final Aes256Component aes256Component;
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        Key aesKey = new SecretKeySpec(this.key.getBytes(), "AES");
+        if (ObjectUtils.isEmpty(attribute)) {
+            return attribute;
+        }
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-            return new String(Base64.getEncoder().encode(cipher.doFinal(attribute.getBytes())));
+            return aes256Component.encrypt(attribute);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("failed to encrypt data", e);
         }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        Key aesKey = new SecretKeySpec(this.key.getBytes(), "AES");
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, aesKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+            return aes256Component.decrypt(dbData);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            return dbData;
         }
     }
+
 }

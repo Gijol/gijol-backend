64baf0780ec133d5982ed8ce431bc77ccb78e600
grace_goose <choieungi@gm.gist.ac.kr>
2023-06-03 19:19:38 +0900

    feat: auditing option 추가

--- src/main/java/com/gist/graduation/GraduationApplication.java
+++ src/main/java/com/gist/graduation/GraduationApplication.java
@@ -2,11 +2,13 @@ package com.gist.graduation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GraduationApplication {
 
     public static void main(String[] args) {
